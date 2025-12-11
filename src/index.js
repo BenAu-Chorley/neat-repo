cc219450d4ec4a9d71ce521be168a817412c3c7b
Ben Au @ Chorley <ben.au@chorley.gov.uk>
2025-12-11 15:53:04 +0000

    Applied urgent fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 console.log('Testing 2');
 console.log(['apple', 'banana', 'cherry', 'durian'].filter(f => f.indexOf('e') >= 0).join(','));
+
+console.log('Sudden urgent fix request comes in. Need to be dealth with immediately.');
+console.log('... handling ...');
+console.log('Fix completed');
